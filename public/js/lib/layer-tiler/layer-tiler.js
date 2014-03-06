c2c69363947ec44064fd4284ae0f90ebed71b2a6
dimik <dimik@yandex.ru>
2014-03-06 23:21:00 +0700

    remove tile-source part

--- public/js/lib/layer-tiler/layer-tiler.js
+++ public/js/lib/layer-tiler/layer-tiler.js
@@ -46,28 +46,39 @@ var LayerTiler = module.exports = inherit(/** @lends LayerTiler prototype. */ {
     render: function () {
         var defer = new Vow.Deferred(),
             options = this._options,
+            source = this._source,
             minZoom = ~~options.minZoom,
+            maxZoom = isFinite(options.maxZoom)? ~~options.maxZoom : source.getZoomBySource(),
             handlers = [
                 this._createFolder.bind(this, options.output)
-            ],
-            maxZoom = isFinite(options.maxZoom)? ~~options.maxZoom : this.getZoomBySource(),
-            requests = handlers.length;
+            ];
 
         this._state = 'processing';
 
         for(var zoom = maxZoom; zoom >= minZoom; zoom--) {
+            var tilesCount = source.getTilesNumberAtZoom(zoom),
+                folderName = path.join(options.output, zoom.toString(10));
+
             handlers.push(
-                this._createFolder.bind(this, path.join(options.output, zoom.toString(10))),
-                this.renderTilesAtZoom.bind(this, zoom)
+                this._createFolder.bind(this, folderName)
             );
-            requests += this._getTilesAmountAtZoom(zoom) + 1;
+
+            for(var x = 0; x < tilesCount; x++) {
+                for(var y = 0; y < tilesCount; y++) {
+                    if(source.isTileFound(x, y, zoom)) {
+                        handlers.push(
+                            this.renderTile.bind(this, x, y, zoom)
+                        );
+                    }
+                }
+            }
         }
 
-        var num = requests;
+        var num = handlers.length;
 
         this._resolveAllHandlers(handlers)
             .progress(function (message) {
-                defer.notify({ message: message, progress: 100 - Math.floor(--num * 100 / requests) });
+                defer.notify({ message: message, progress: 100 - Math.floor(--num * 100 / handlers.length) });
             })
             .done(defer.resolve, defer.reject, defer);
 
@@ -91,7 +102,7 @@ var LayerTiler = module.exports = inherit(/** @lends LayerTiler prototype. */ {
     renderTile: function (x, y, zoom) {
         var defer = new Vow.Deferred();
 
-        this.createTile(x, y, zoom)
+        this._source.getTile(x, y, zoom)
             .save(
                 this.getTileUrl(x, y, zoom),
                 this._options.tileType
@@ -102,146 +113,6 @@ var LayerTiler = module.exports = inherit(/** @lends LayerTiler prototype. */ {
             });
 
         return defer.promise();
-    },
-    createTile: function (x, y, zoom) {
-        var tileSize = this._options.tileSize,
-            globalPixelPoint = [ x * tileSize, y * tileSize ],
-            topLeft = this.fromGlobalPixels(
-                globalPixelPoint,
-                zoom
-            ),
-            bottomRight = this.fromGlobalPixels(
-                [ globalPixelPoint[0] + tileSize, globalPixelPoint[1] + tileSize ],
-                zoom
-            ),
-            offset = [ bottomRight[0] - topLeft[0], bottomRight[1] - topLeft[1] ],
-            tileOffset = [
-                topLeft[0] === 0? tileSize - offset[0] : 0,
-                topLeft[1] === 0? tileSize - offset[1] : 0
-            ];
-
-        if(zoom === 0) {
-            tileOffset = [
-                Math.round((tileSize - offset[0]) / 2),
-                Math.round((tileSize - offset[1]) / 2)
-            ];
-        }
-
-        return this._source
-            .cropTo(
-                new Tile(tileSize, tileSize),
-                topLeft[0],
-                topLeft[1],
-                offset[0],
-                offset[1],
-                tileOffset[0],
-                tileOffset[1],
-                offset[0],
-                offset[1]
-            );
-    },
-    /**
-     * @function
-     * @name LayerTiler.resizeSourceAtZoom
-     * @param {Number} zoom
-     */
-    resizeSourceAtZoom: function (zoom) {
-        var source = this._source,
-            tileSize = this._options.tileSize,
-            tilesSize = this.getTilesNumberAtZoom(zoom) * tileSize,
-            curZoom = this.getZoomBySource(),
-            curTilesSize = this.getTilesNumberAtZoom(curZoom) * tileSize,
-            coef = Math.pow(2, curZoom - zoom),
-            width = source.getWidth(), height = source.getHeight(),
-            curLongestSideSize = Math.max(width, height),
-            curOffset = curTilesSize - curLongestSideSize,
-            offset = Math.round(curOffset / coef),
-            longestSideSize = tilesSize - offset,
-            proportion = curLongestSideSize / Math.min(width, height);
-
-        if(source.getWidth() > source.getHeight()) {
-            this._source
-                .resize(longestSideSize, Math.round(longestSideSize / proportion));
-        }
-        else {
-            this._source
-                .resize(Math.round(longestSideSize / proportion), longestSideSize);
-        }
-    },
-    /**
-     * Calculate zoom value by source size.
-     * @function
-     * @name LayerTiler.getZoomBySource
-     * @returns {Number} Zoom value.
-     */
-    getZoomBySource: function () {
-        var source = this._source,
-            tileSize = this._options.tileSize,
-            log2 = function (n) {
-                return Math.log(n) / Math.log(2);
-            },
-            tilesCount = Math.ceil(
-                Math.max(
-                    Math.ceil(source.getWidth() / tileSize),
-                    Math.ceil(source.getHeight() / tileSize)
-                )
-            );
-
-        return Math.ceil(log2(tilesCount));
-    },
-    /**
-     * Calculate number of tiles at the current zoom.
-     * @name LayerTiler.getTilesNumberAtZoom
-     * @param {Number} zoom
-     * @returns {Number} Tiles count at zoom.
-     */
-    getTilesNumberAtZoom: function (zoom) {
-        return Math.pow(2, zoom);
-    },
-    /**
-     * @function
-     * @name LayerTiler.renderTilesAtZoom
-     * @param {Number} zoom
-     * @returns {Vow} Promise A+.
-     */
-    renderTilesAtZoom: function (zoom) {
-        var source = this._source,
-            tilesCount = this.getTilesNumberAtZoom(zoom),
-            handlers = [];
-
-        if(zoom < ~~this._options.maxZoom) {
-            this.resizeSourceAtZoom(zoom);
-        }
-
-        for(var x = 0; x < tilesCount; x++) {
-            for(var y = 0; y < tilesCount; y++) {
-                if(this.isTileFound(source.getWidth(), source.getHeight(), x, y, zoom)) {
-                    handlers.push(
-                        this.renderTile.bind(this, x, y, zoom)
-                    );
-                }
-            }
-        }
-
-        return this._resolveAllHandlers(handlers);
-    },
-    _getTilesAmountAtZoom: function (zoom) {
-        var source = this._source,
-            tilesCount = this.getTilesNumberAtZoom(zoom),
-            maxZoom = this._options.maxZoom,
-            width = maxZoom > zoom? source.getWidth() / 2 * (maxZoom - zoom) : source.getWidth(),
-            height = maxZoom > zoom? source.getHeight() / 2 * (maxZoom - zoom) : source.getHeight(),
-            amount = 0;
-
-        for(var x = 0; x < tilesCount; x++) {
-            for(var y = 0; y < tilesCount; y++) {
-                if(this.isTileFound(width, height, x, y, zoom)) {
-                    amount++;
-                }
-            }
-        }
-
-        return amount;
     },
     /**
      * Folders creation helper.
@@ -265,59 +136,6 @@ var LayerTiler = module.exports = inherit(/** @lends LayerTiler prototype. */ {
         });
 
         return defer.promise();
-    },
-    /**
-     * Check if we need to render this tile.
-     * @function
-     * @name LayerTiler.isTileFound
-     * @param {Number} width Source width.
-     * @param {Number} height Source heigth.
-     * @param {Number} x Tile coordinate by X.
-     * @param {Number} y Tile coordinate by Y.
-     * @param {Number} zoom
-     * @returns {Boolean}
-     */
-    isTileFound: function (width, height, x, y, zoom) {
-        var tileSize = this._options.tileSize,
-            tilesCount = this.getTilesNumberAtZoom(zoom),
-            tilesCountByWidth = Math.ceil(width / tileSize / 2) * 2,
-            tilesCountByHeight = Math.ceil(height / tileSize / 2) * 2,
-            offset = [
-                (tilesCount - tilesCountByWidth) / 2,
-                (tilesCount - tilesCountByHeight) / 2
-            ];
-
-        return (x >= offset[0] && x < tilesCountByWidth + offset[0]) &&
-            (y >= offset[1] && y < tilesCountByHeight + offset[1]);
-    },
-    /**
-     * Get source point coordinates by global tile coordinates at current zoom.
-     * @function
-     * @name LayerTiler.fromGlobalPixels
-     * @param {Number[]} globalPixelPoint Global pixel coordinates.
-     * @param {Number} zoom
-     * @returns {Number[]} Coordinates local to source image.
-     */
-    fromGlobalPixels: function (globalPixelPoint, zoom) {
-        var source = this._source,
-            tileSize = this._options.tileSize,
-            tilesCount = this.getTilesNumberAtZoom(zoom),
-            pixelsCount = tilesCount * tileSize,
-            offset = [
-                Math.ceil((pixelsCount - source.getWidth()) / 2),
-                Math.ceil((pixelsCount - source.getHeight()) / 2)
-            ];
-
-        return [
-            Math.min(
-                Math.max(globalPixelPoint[0] - offset[0], 0),
-                source.getWidth()
-            ),
-            Math.min(
-                Math.max(globalPixelPoint[1] - offset[1], 0),
-                source.getHeight()
-            )
-        ];
     },
     /**
      * Process handlers in sequence.
