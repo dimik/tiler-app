27a74845146b7e95d3c385e1c242212239c0d5d2
dimik <dimik@yandex.ru>
2014-03-11 12:11:12 +0700

    add support for tileBackground and opacity

--- public/js/app.js
+++ public/js/app.js
@@ -35,11 +35,12 @@ requirejs.config({
                 center: [0, 0],
                 zoom: 2,
                 behaviors: ['default', 'scrollZoom'],
-                controls: ['zoomControl', 'fullscreenControl']
+                controls: ['zoomControl', 'fullscreenControl'/*, 'typeSelector'*/]
             },
             options: {
                 // zoomControlSize: 'small',
-                fullscreenControlSize: 'default'
+                fullscreenControlSize: 'default',
+                typeSelectorFloat: 'left'
             }
         },
         'layer-settings-map-view': {
@@ -80,58 +81,61 @@ require([
 ], function (LayerTiler, MapView, ImageReaderMapView, LayerSettingsMapView, PopupMapView, PreloaderMapView) {
     var output = 'tiles-' + Date.now(),
         tiler = new LayerTiler({ output: output }),
+        tileSource = tiler.getTileSource(),
         mapView = new MapView(),
         map = mapView.getMap(),
         reader = new ImageReaderMapView(map),
         settings = new LayerSettingsMapView(map),
         popup = new PopupMapView(map),
-        preloader = new PreloaderMapView(map);
+        preloader = new PreloaderMapView(map),
+        sourceData;
 
     reader.render();
 
     reader.events.on('load', function (e) {
         tiler.openSource(URL.createObjectURL(e.source))
             .then(function (res) {
+                sourceData = {
+                    src: res.src,
+                    name: e.source.name,
+                    type: e.source.type,
+                    size: e.source.size,
+                    width: res.width,
+                    height: res.height
+                };
+
                 URL.revokeObjectURL(e.source);
                 reader.hide();
 
-                var source = tiler.getTileSource();
-
-                settings.render({
-                    source: {
-                        src: res.src,
-                        name: e.source.name,
-                        type: e.source.type,
-                        size: e.source.size,
-                        width: res.width,
-                        height: res.height
-                    },
-                    settings: {
-                        output: output,
-                        minZoom: 0,
-                        maxZoom: source.getZoomBySource(),
-                        georeferenced: false,
-                        tileType: e.source.type
-                    }
-                });
-
-                mapView.render(source, {
-                    name: output,
-                    minZoom: 0,
-                    maxZoom: source.getZoomBySource(),
+                tileSource.setOptions({
                     tileType: e.source.type
                 });
+
+                var sourceOptions = tileSource.getOptions();
+
+                settings.render({
+                    source: sourceData,
+                    settings: jQuery.extend({}, sourceOptions, {
+                        output: output,
+                        tileOpacity: Math.floor(sourceOptions.tileOpacity * 100),
+                        georeferenced: false,
+                    })
+                });
+
+                mapView.render(tileSource, jQuery.extend({
+                    name: output,
+                    tileType: e.source.type
+                }, tileSource.getOptions()));
             });
 
         settings.events
             .on('submit', function (e) {
-                console.log(e.settings);
                 preloader.render({
                     progress: 0
                 });
 
                 tiler
-                    .setOptions(e.settings)
+                    .setOptions({ output: e.settings.name })
                     .render()
                     .progress(function (v) {
                         preloader.render(v);
@@ -142,6 +146,21 @@ require([
                             content: reason
                         });
                     });
+            })
+            .on('change', function (e) {
+                tileSource.setOptions({
+                    tileBackground: e.settings.tileBackground,
+                    minZoom: ~~e.settings.minZoom,
+                    maxZoom: ~~e.settings.maxZoom,
+                    tileType: e.settings.tileType,
+                    tileOpacity: e.settings.tileOpacity / 100
+                });
+                /*
+                settings.render({
+                    source: sourceData,
+                    settings: e.settings
+                });
+                */
             })
             .on('cancel', function () {
                 tiler.cancel();
