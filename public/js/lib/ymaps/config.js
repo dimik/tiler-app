40f0bd6612a19c82f16e5dd837c3048c92a0d48a
dimik <dimik@yandex.ru>
2014-03-23 14:01:16 +0400

    конфиг подключения АПИ Карт

---
+++ public/js/lib/ymaps/config.js
@@ -0,0 +1,10 @@
+modules.define('ymaps-config', function (provide) {
+    provide({
+        url: 'http://api-maps.yandex.ru/2.1-dev/',
+        params: {
+            lang: 'ru-RU',
+            load: 'package.full',
+            mode: 'debug'
+        }
+    });
+});
