5f0bec67f4555f47c4b5728a12974b83e8a4c0aa
dimik <dimik@yandex.ru>
2014-03-24 17:09:19 +0400

    https for ya-disk

--- public/js/lib/yandex-disk/config.js
+++ public/js/lib/yandex-disk/config.js
@@ -1,7 +1,7 @@
 modules.define('yandex-disk-config', function (provide) {
 
     provide({
-        url: 'http://127.0.0.1:8002',
+        url: 'https://127.0.0.1:8002/yandex-disk-api',
         home: '/',
         token: 'ad47a7d4279b4edaaf1bd3e8c91d4539'
     });
