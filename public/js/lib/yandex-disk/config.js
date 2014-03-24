dd51f9d459536b69e4e59ec6a7df93007467d625
dimik <dimik@yandex.ru>
2014-03-24 23:14:12 +0400

    new host for disk api

--- public/js/lib/yandex-disk/config.js
+++ public/js/lib/yandex-disk/config.js
@@ -1,7 +1,7 @@
 modules.define('yandex-disk-config', function (provide) {
 
     provide({
-        url: 'https://127.0.0.1:8002/yandex-disk-api',
+        url: 'http://127.0.0.1:8090/',
         home: '/',
         token: 'ad47a7d4279b4edaaf1bd3e8c91d4539'
     });
