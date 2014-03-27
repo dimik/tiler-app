edc0c6f47f301cff175b1c8fec77f02a51a8aa3f
dimik <dimik@yandex.ru>
2014-03-27 20:40:05 +0400

    ymaps option Manager with mapping

---
+++ public/js/lib/ymaps/option/manager.js
@@ -0,0 +1,17 @@
+modules.define('ymaps-option.manager', [
+    'ymaps',
+    'ymaps-option-mapper'
+], function (provide, ymaps, mapper) {
+
+    function OptionManager(options, parent, name) {
+        var manager = new ymaps.option.Manager(options, parent, name);
+
+        manager.resolve = function (key, name) {
+            return this.get(mapper.resolve(key, name));
+        };
+
+        return manager;
+    }
+
+    provide(OptionManager);
+});
