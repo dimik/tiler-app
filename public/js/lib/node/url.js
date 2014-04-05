be8f6d8d37b352ca0792661da5731005b3d064ee
dimik <dimik@yandex.ru>
2014-04-05 15:14:15 +0400

    very simple implementation of the node-url

---
+++ public/js/lib/node/url.js
@@ -0,0 +1,24 @@
+modules.define('node-url', function (provide) {
+
+    provide({
+        parse: function (s) {
+            var re = /(\w+:)?\/\/([^\/]+)(([^\?]+)\??(.*))/;
+                matches = re.exec(re),
+                getMatch = function (index) {
+                    return matches[index] || '';
+                },
+                query = getMatch(5);
+
+            return {
+                href: getMatch(0),
+                protocol: getMatch(1),
+                host: getMatch(2),
+                hostname: getMatch(2),
+                path: getMatch(3),
+                pathname: getMatch(4),
+                search: query.length? '?' + query : '',
+                query: query
+            };
+        }
+    });
+});
